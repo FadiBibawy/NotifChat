473196ab86ead69078e65b6bc0a7a12b63b1b85e
FadiBibawy <eng.fadimichel@gmail.com>
2023-11-30 00:11:32 +0100

    api users controller & routes & migrated messages

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,13 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2023_11_26_153051) do
+ActiveRecord::Schema[7.0].define(version: 2023_11_26_155805) do
+  create_table "api_v1_messages", force: :cascade do |t|
+    t.text "content"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
   create_table "jwt_denylist", force: :cascade do |t|
     t.string "jti", null: false
     t.datetime "exp", null: false
